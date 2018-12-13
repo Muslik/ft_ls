368a16b35c886276c37fa60b4db507a07b4b1575
Nigel Archibald yronwood <narchiba@e2r13p5.21-school.ru>
2018-12-13 08:32:11 +0300

    ft_strjoin.c

--- libft/srcs/ft_strjoin.c
+++ libft/srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dmorgil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 19:07:35 by dmorgil           #+#    #+#             */
-/*   Updated: 2018/12/12 22:44:09 by dmorgil          ###   ########.fr       */
+/*   Updated: 2018/12/13 08:31:00 by narchiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 char	*ft_strjoin(char const *s1, char const *s2, int free_opt)
 {
 	char	*newstr;
+	size_t	len1;
+	size_t	len2;
 
 	if (!s1 || !s2)
 		return (NULL);
-	newstr = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	newstr = ft_strnew(len1 + len2);
 	if (!newstr)
 		return (NULL);
-	ft_memmove(newstr, s1, ft_strlen(s1));
-	ft_memmove(newstr + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	ft_memmove(newstr, s1, len1);
+	ft_memmove(newstr + len1, s2, len2 + 1);
 	if (free_opt == 1)
 		free((char *)s1);
 	if (free_opt == 2)
@@ -34,4 +38,4 @@ char	*ft_strjoin(char const *s1, char const *s2, int free_opt)
 		free((char *)s2);
 	}
 	return (newstr);
-}
+
